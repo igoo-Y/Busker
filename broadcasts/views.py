4e78c483c0376a5d0b6badd43cb22a88e5a6d3a5
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-06-29 14:21:38 +0900

    django-channels install

--- broadcasts/views.py
+++ broadcasts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Broadcast
 
+
 def home(request):
     # broadcasts = Broadcast.objects.all()
     return render(request, "broadcasts/broadcasts_list.html")
 
 
 def broadcast(request):
-    pass
+    return render(request, "broadcasts/main.html")
